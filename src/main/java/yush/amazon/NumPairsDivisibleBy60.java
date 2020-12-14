880938e4f079519b699bdbf93a3ba21d2ed7e35f
yushen <yushen@192.168.0.103>
2020-12-14 09:45:00 -0500

    More

---
+++ src/main/java/yush/amazon/NumPairsDivisibleBy60.java
@@ -0,0 +1,24 @@
+package yush.amazon;
+
+/*
+ * You are given a list of songs where the ith song has a duration of time[i] seconds.
+ * 
+ * Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j 
+ * such that i < j with (time[i] + time[j]) % 60 == 0.
+ */
+public class NumPairsDivisibleBy60 {
+	public int numPairsDivisibleBy60(int[] time) {
+		if (time.length < 2) {
+			return 0;
+		}
+
+		int[] cnt = new int[60];
+		int result = 0;
+		for (int t : time) {
+			result += cnt[(60 - t % 60) % 60];
+			cnt[t % 60] += 1;
+		}
+		
+		return result;
+	}
+}
